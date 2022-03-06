51351c1d309cd18c30c2628ef9b409ec7af34ff7
Dennis Lövkvist <dennis.lovkvist@protonmail.com>
2022-03-06 14:57:54 +0100

    2 stepper motors

--- main.c
+++ main.c
@@ -1,16 +1,36 @@
+#include <stdlib.h>
 #include "pico/stdlib.h"
-
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
 const int STEPS[4][4] = {{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
 const int STEPS_FULL_ROTATION = 2060;
 
 typedef struct Motor Motor;
+typedef struct Joystick Joystick;
 
 struct Motor
 {    
     int gpio_step, world_step, increment;
-    uint min,max;
     uint GPIO_PINS[4];
 };
+struct Joystick
+{    
+    uint GPIO_PIN_VRX;
+    uint GPIO_PIN_VRY;
+};
+void init_joystick(Joystick *joystick)
+{
+    
+    //gpio_init(joystick->GPIO_PIN_VRX);  
+    //gpio_init(joystick->GPIO_PIN_VRY);    
+    //gpio_set_dir(joystick->GPIO_PIN_VRX, GPIO_IN);  
+    //gpio_set_dir(joystick->GPIO_PIN_VRY, GPIO_IN);  
+}
+void define_joystick(Joystick *joystick, uint vrx, uint vry)
+{
+    joystick->GPIO_PIN_VRX = vrx;
+    joystick->GPIO_PIN_VRY = vry;
+}
 void define_motor(Motor *motor, uint pin_0, uint pin_1, uint pin_2, uint pin_3, int increment)
 {
     motor->GPIO_PINS[0] = pin_0;
@@ -18,6 +38,7 @@ void define_motor(Motor *motor, uint pin_0, uint pin_1, uint pin_2, uint pin_3, 
     motor->GPIO_PINS[2] = pin_2;
     motor->GPIO_PINS[3] = pin_3;
     motor->increment = increment;
+    motor->world_step = 0;
 }
 void init_motor(Motor *motor)
 {
@@ -29,7 +50,7 @@ void init_motor(Motor *motor)
 }
 void update_motor(Motor *motor)
 {
-    int *step = &motor->gpio_step;
+    int *step = &(motor->gpio_step);
     for (size_t i = 0; i < 4; i++)
     {
         gpio_put(motor->GPIO_PINS[i], STEPS[*step][i]);
@@ -44,22 +65,71 @@ void update_motor(Motor *motor)
     } 
     motor->world_step += motor->increment;
 }
+int control_motor(int adc, int invert)
+{
+    adc_select_input(adc);
+    uint16_t t1 = adc_read();
 
+    if(abs(t1 -2048) <400)
+    {
+        return 0;
+    }
+    else
+    {
+        if(t1 < 2010)
+        {
+            return (invert) ? -1:1;
+        }
+        else
+        {
+            return (invert) ? 1:-1;
+        }
+    }
+}
 int main() 
 {       
-    Motor motors[2];
+   
 
-    define_motor(&(motors[0]),18,19,20,21,1);
-    define_motor(&(motors[1]),10,11,12,13,1);
+    stdio_init_all();
 
-    init_motor(&(motors[0]));
-    init_motor(&(motors[1]));
+    Motor motor_1;
+    Motor motor_2;
+    
+    //define_motor(&motor_0,13,12,11,10,1);
+    define_motor(&motor_1,18,19,20,21,1);
+    define_motor(&motor_2,13,12,11,10,1);
 
-    while(true)
-    { 
-        //update_motor(&(motors[0]));
-        update_motor(&(motors[1]));
-        sleep_ms(4); 
+    init_motor(&motor_1);
+    init_motor(&motor_2);
+    
+
+    
+    adc_init();
+    adc_gpio_init(27);
+    adc_gpio_init(28);
+
+    gpio_init(16);    
+    gpio_set_dir(16, 0);  
+    gpio_pull_up(16);
+
+     gpio_init(1);    
+    gpio_set_dir(1, 1);  
+
+    while (true) 
+    {
+
+        
+                sleep_ms(3);
+                if(motor_1.increment != 0)update_motor(&motor_1);
+                sleep_ms(3);
+                if(motor_2.increment != 0)update_motor(&motor_2);
+
+                motor_1.increment = control_motor(1,0);
+                motor_2.increment = control_motor(2,0);
+        
+        
+        
+        
     }
 }
 
